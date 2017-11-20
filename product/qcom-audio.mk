31f4a7ce2e485332208327e27751bc65e20f4177
danile71 <danile71@gmail.com>
2017-11-20 22:24:37 +0300

    Serrano VE: fixed keylight, OSS audio, IR
    Change-Id: Id08fbf411500bee6620626bda463e6463f9fb20a

--- product/qcom-audio.mk
+++ product/qcom-audio.mk
@@ -1,6 +1,8 @@
 # Audio configuration
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml
+    $(LOCAL_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml \
+    $(LOCAL_PATH)/configs/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml
+
 
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
