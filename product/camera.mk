cbe1e5aa1c605f4a3bebb59145a1dd887a35ddf6
danile71 <danile71@gmail.com>
2018-06-03 14:39:56 +0300

    serranovexx-common: Build Camera2
    Change-Id: I560fd3d2495161f3299d8f86c16363acbb545590

--- product/camera.mk
+++ product/camera.mk
@@ -1,8 +1,3 @@
 # Camera
 PRODUCT_PACKAGES += \
-    libqc-opt \
-    Snap
-
-# Media (camera) configuration files
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/configs/media/media_profiles.xml:system/etc/media_profiles.xml
+    Camera2
