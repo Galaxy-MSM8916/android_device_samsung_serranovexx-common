202e3dc4a383df3b1dc6f8b73db21d7bd91dcc88
Arne Coucheron <arco68@gmail.com>
2018-10-17 22:32:30 +0300

    serranovexx-common: Set the first API level the device shipped on
    Change-Id: I83202b071cda1ea753c3ad44a3910ba23742ed56

--- board/device-init.mk
+++ board/device-init.mk
@@ -1,2 +1,5 @@
 # Init
 TARGET_LIBINIT_MSM8916_DEFINES_FILE := $(LOCAL_PATH)/init/init_serranovexx.cpp
+
+# The first api level the device has commercially launched on
+PRODUCT_SHIPPING_API_LEVEL := 19
